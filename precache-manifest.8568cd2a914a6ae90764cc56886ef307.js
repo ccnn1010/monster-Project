dc63ce43eb63fb1ec573acfa04ff02a628014de9
Caterina <ccnn1010@gmail.com>
2020-05-10 01:42:58 +1000

    Updates

---
+++ precache-manifest.8568cd2a914a6ae90764cc56886ef307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22acf1ddf57fdeb09206c8edb0695f67",
+    "url": "/monster-project/index.html"
+  },
+  {
+    "revision": "8e5fd82c8d3e1a0b35c9",
+    "url": "/monster-project/static/css/main.92ae0a4c.chunk.css"
+  },
+  {
+    "revision": "2d51c685ce31942fce6b",
+    "url": "/monster-project/static/js/2.6478ef4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-project/static/js/2.6478ef4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5fd82c8d3e1a0b35c9",
+    "url": "/monster-project/static/js/main.79315912.chunk.js"
+  },
+  {
+    "revision": "b889e70412a77f80b155",
+    "url": "/monster-project/static/js/runtime-main.3dc34f3e.js"
+  }
+]);
